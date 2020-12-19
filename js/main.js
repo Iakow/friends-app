b96e8f2d590eb18bb0aa0d7241bfd5b77ea6434e
Yakov <ponomarykov@gmail.com>
2020-12-20 01:54:58 +0200

    upgrade card styles

--- js/main.js
+++ js/main.js
@@ -7,16 +7,20 @@ fetch(`https://randomuser.me/api/?results=${cardsAmount}`)
   .then(response => response.json())
   .then(response => {
     const users = response.results; // array
+    console.log(users);
 
     users.forEach((user, index) => {
       const card = document.createElement('li');
       card.className = 'card';
-      card.innerHTML = (
-        `<img src="${user.picture.large}" height="128" width="128">
-         <p>${user.name.first} ${user.name.last}, ${user.dob.age}</p>
-         <p>${user.location.country}, ${user.location.city}</p>
-         <img src="https://www.countryflags.io/${user.nat}/flat/24.png">`
-      )
+      const flag = `<img src="https://www.countryflags.io/${user.nat}/flat/24.png">`;
+      card.innerHTML = ([
+        `<div class="card-cover" style="background-image: url('${user.picture.large}')"></div>`,
+        `<img class="card-photo" src="${user.picture.large}" height="128" width="128">`,
+        `<p class="card-name">${user.name.first} ${user.name.last}, ${user.dob.age}</p>`,
+        `<p class="card-addr">${flag}${user.location.country}, ${user.location.city}</p>`,
+        `<p class="card-phone">${user.phone}</p>`,
+        `<p class="card-email">${user.email}</p>`
+      ].join('\n'))
 
       allCardElements[index] = card;
     })
