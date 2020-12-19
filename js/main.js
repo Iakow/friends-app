ddf9c137f47645497b1910194dddeeb9b32b6fdc
Yakov <ponomarykov@gmail.com>
2020-12-20 00:20:35 +0200

    probe add fetching data and use placeholders

--- js/main.js
+++ js/main.js
@@ -1,30 +1,49 @@
 'use strict';
 
-fetch('https://randomuser.me/api/?results=2')
+const cardsAmount = 20;
+const allCardElements = [];
+
+fetch(`https://randomuser.me/api/?results=${cardsAmount}`)
   .then(response => response.json())
   .then(response => {
-    const users = response.results;
-    console.log(users)
-    /* 
-    user.picture.large;
-    user.name.first + ' ' + user.name.last + ', ' + user.dob.age;
-    /user.nat/ user.location.country + ', ' + user.location.city;
-    lag.src = `https://www.countryflags.io/${user.nat}/flat/24.png`;
-    */
+    const users = response.results; // array
+
+    users.forEach((user, index) => {
+      const card = document.createElement('li');
+      card.className = 'card';
+      card.innerHTML = (
+        `<img src="${user.picture.large}" height="128" width="128">
+         <p>${user.name.first} ${user.name.last}, ${user.dob.age}</p>
+         <p>${user.location.country}, ${user.location.city}</p>
+         <img src="https://www.countryflags.io/${user.nat}/flat/24.png">`
+      )
+
+      allCardElements[index] = card;
+    })
+
+    function mountCards() {
+      const mountPoint = document.querySelector('.main-container');
+      mountPoint.innerHTML = '';
+      const fragment = document.createDocumentFragment();
+
+      allCardElements.forEach(cardEl => {
+        fragment.append(cardEl);
+      })
+
+      mountPoint.append(fragment)
+    }
+
+    mountCards();
   });
 
-function mountCards() {
-  const mountPoint = document.querySelector('.main-container');
-  const fragment = document.createDocumentFragment();
+const mountPoint = document.querySelector('.main-container');
+const fragment = document.createDocumentFragment();
 
-  
-  for (let i = 1; i <= 20; i++) {
-    const card = document.createElement('li');
-    card.className = 'card';
-    fragment.append(card);
-  }
+for (let i = 1; i <= cardsAmount; i++) {
+  const card = document.createElement('li');
+  card.className = 'card';
 
-  mountPoint.append(fragment);
+  fragment.append(card);
 }
 
-mountCards();
+mountPoint.append(fragment);
