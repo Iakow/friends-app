31921458c69a2d0108ac712f05176e9bb82b9dfd
Yakov <ponomarykov@gmail.com>
2020-12-19 14:47:47 +0200

    generation cards from js

--- js/main.js
+++ js/main.js
@@ -12,3 +12,19 @@ fetch('https://randomuser.me/api/?results=2')
     lag.src = `https://www.countryflags.io/${user.nat}/flat/24.png`;
     */
   });
+
+function mountCards() {
+  const mountPoint = document.querySelector('.main-container');
+  const fragment = document.createDocumentFragment();
+
+  
+  for (let i = 1; i <= 20; i++) {
+    const card = document.createElement('div');
+    card.className = 'card';
+    fragment.append(card);
+  }
+
+  mountPoint.append(fragment);
+}
+
+mountCards();
