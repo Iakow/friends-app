0c7c50745e6ae4046f9017086ba0209dd004a239
Yakov <ponomarykov@gmail.com>
2020-12-20 22:15:09 +0200

    implement sort by age

--- js/main.js
+++ js/main.js
@@ -1,53 +1,99 @@
 'use strict';
 
-const cardsAmount = 20;
+const cardsAmount = 200;
 const allCardElements = [];
+
+(function mountPlaceHolders() {
+  const mountPoint = document.querySelector('.main-container');
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 1; i <= cardsAmount; i++) {
+    const card = document.createElement('li');
+    card.className = 'card';
+
+    fragment.append(card);
+  }
+
+  mountPoint.append(fragment);
+})();
+
+/* монтируеум из allCardElements */
+function mountCards(arr) {
+  const mountPoint = document.querySelector('.main-container');
+  /* очищаем main-container */
+  mountPoint.innerHTML = '';
+  const fragment = document.createDocumentFragment();
+
+  arr.forEach(cardEl => {
+    fragment.append(cardEl);
+  })
+
+  mountPoint.append(fragment)
+}
 
 fetch(`https://randomuser.me/api/?results=${cardsAmount}`)
   .then(response => response.json())
   .then(response => {
-    const users = response.results; // array
-    console.log(users);
+    const users = response.results;
 
-    users.forEach((user, index) => {
+    /* строим allCardElements */
+    users.forEach((userData, index) => {
       const card = document.createElement('li');
       card.className = 'card';
-      const flag = `<img src="https://www.countryflags.io/${user.nat}/flat/24.png">`;
+      card.userData = userData;
+
+      const flag = `<img src="https://www.countryflags.io/${userData.nat}/flat/24.png">`;
       card.innerHTML = ([
-        `<div class="card-cover" style="background-image: url('${user.picture.large}')"></div>`,
-        `<img class="card-photo" src="${user.picture.large}" height="128" width="128">`,
-        `<p class="card-name">${user.name.first} ${user.name.last}, ${user.dob.age}</p>`,
-        `<p class="card-addr">${flag}${user.location.country}, ${user.location.city}</p>`,
-        `<p class="card-phone">${user.phone}</p>`,
-        `<p class="card-email">${user.email}</p>`
+        `<div class="card-cover" style="background-image: url('${userData.picture.large}')"></div>`,
+        `<img class="card-photo" src="${userData.picture.large}" height="128" width="128">`,
+        `<p class="card-name">${userData.name.first} ${userData.name.last}, ${userData.dob.age}</p>`,
+        `<p class="card-addr">${flag}${userData.location.country}, ${userData.location.city}</p>`,
+        `<p class="card-phone">${userData.phone}</p>`,
+        `<p class="card-email">${userData.email}</p>`
       ].join('\n'))
 
       allCardElements[index] = card;
     })
 
-    function mountCards() {
-      const mountPoint = document.querySelector('.main-container');
-      mountPoint.innerHTML = '';
-      const fragment = document.createDocumentFragment();
-
-      allCardElements.forEach(cardEl => {
-        fragment.append(cardEl);
-      })
-
-      mountPoint.append(fragment)
-    }
-
-    mountCards();
+    mountCards(allCardElements);
   });
 
-const mountPoint = document.querySelector('.main-container');
-const fragment = document.createDocumentFragment();
+function sortByAge() {
+  const newArr = [...allCardElements].sort((user1, user2) => {
+    //return user1.userData.dob.age - user2.userData.dob.age // это по возрастающей
+    return user2.userData.dob.age - user1.userData.dob.age // это по убывающей
+  })
 
-for (let i = 1; i <= cardsAmount; i++) {
-  const card = document.createElement('li');
-  card.className = 'card';
-
-  fragment.append(card);
+  mountCards(newArr);
 }
 
-mountPoint.append(fragment);
+const selection = {
+  ageSorting: 0,
+
+  sortByAge() {
+    let sortFunc;
+    if (selection.ageSorting === 0) {
+      selection.ageSorting = 1;
+      sortFunc = (user1, user2) => +user2.userData.dob.age - +user1.userData.dob.age;
+    } else if (selection.ageSorting === 1) {
+      selection.ageSorting = -1;
+      sortFunc = (user1, user2) => +user1.userData.dob.age - +user2.userData.dob.age;
+    } else {
+      selection.ageSorting = 0;
+      sortFunc = (user1, user2) => 0;
+    }
+
+    const newArr = [...allCardElements].sort(sortFunc);
+
+    mountCards(newArr);
+    console.log(selection.ageSorting)
+  }
+}
+
+document.querySelector('#sort-age').addEventListener('click', selection.sortByAge);
+
+/* ок, далее. Где хранить данные сортировок и фильтров?
+   Думал о замыкании, но сортировка по имени должна ж сбрасывать сортировку по возрасту
+   поэтому они должны храниться в одном месте.
+   В объекте?
+*/
